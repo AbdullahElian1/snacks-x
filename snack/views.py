17d99ebe322eb0d4a12591ec798844a24c21be88
AbdullahELian1 <abodelian28@gmail.com>
2021-09-01 13:09:48 +0300

    init view

--- snack/views.py
+++ snack/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import  ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
+from .models import Snack
+from django.urls import reverse_lazy
+
