7d1455cf6234091e21268127c1c824425a8fd56c
AbdullahELian1 <abodelian28@gmail.com>
2021-09-01 13:10:36 +0300

    add snack create view

--- snack/views.py
+++ snack/views.py
@@ -8,3 +8,6 @@ class SnackListView(ListView):
     template_name = 'snack_list.html'
     model= Snack
 
+class SnackDetailView(DetailView):
+    template_name= 'snack_detail.html'
+    model= Snack
